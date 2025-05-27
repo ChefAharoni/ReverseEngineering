233554bf5ddfa5ed801730857891871fc116c8a1
Amit Aharoni <amit.aharoni@columbia.edu>
2025-05-27 14:28:21 -0400

    modified strlen

--- dad/arrs.c
+++ dad/arrs.c
@@ -1,14 +1,21 @@
 
 #include <stdio.h>
 
-int strlen_mine(char s[]) {
-	return 0;
+int strlen_mine(char *s) {
+	int n;
+
+	for (n = 0; *s != '\0'; s++)
+		n++;
+
+	return n;
+
 }
 
 int main() {
 	char s[10] = "hello";
 
-	strlen_mine(s);
+	int x = strlen_mine(s);
+	printf("Strlen: %d\n", x);
 
 	return 0;
 }
